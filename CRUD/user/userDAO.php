f6d77cb66b92a2eb219bcbb8301693619b0aefd7
brettnye <brettnye@mail.weber.edu>
2021-02-17 14:07:44 -0700

    Inital Commit

---
+++ CRUD/user/userDAO.php
@@ -0,0 +1,62 @@
+<?php
+class UserDAO {
+  function getUser($user){
+    require_once('./utilities/connection.php');
+    
+    $sql = "SELECT FirstName, LastName, Username, user_id FROM newuser WHERE user_id =" . $user->getUserId();
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user->setFirstName($row["FirstName"]);
+        $user->setLastName($row["LastName"]);
+        $user->setUsername($row["Username"]);
+        echo $result->num_rows;
+    }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+  }
+
+  function createUser($user){
+    require_once('./utilities/connection.php');
+    
+    $sql = "INSERT INTO cs3620_proj.newuser
+    (
+    `Username`,
+    `Password`,
+    `FirstName`,
+    `LastName`)
+    VALUES
+    ('" . $user->getUsername() . "',
+    '" . $user->getPassword() . "',
+    '" . $user->getFirstName() . "',
+    '" . $user->getLastName() . "'
+    );";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "user created";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+  }
+
+  function deleteUser($un){
+    require_once('./utilities/connection.php');
+    
+    $sql = "DELETE FROM cs3620_proj.newuser WHERE Username = '" . $un . "';";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "user deleted";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+  }
+}
+?>
